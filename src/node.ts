1c768bc04b91cbab95053dfd256d5e8f8737d27d
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-04-24 11:55:07 -0400

    plugin now places nodes in play area by default (not currentPage)

--- src/node.ts
+++ src/node.ts
@@ -14,17 +14,20 @@ const src_canvas_first_y = 965, src_canvas_gap = 1783;
 function timer_y(i: number) { return timer_first_y + (i - 1) * timer_gap; }
 function src_canvas_y(i: number) { return src_canvas_first_y + (i - 1) * src_canvas_gap; }
 
+// Get playing area page
+const playPage = figma.root.findChild(node => node.name === 'Play') ?? figma.currentPage;
+
 export function createTimer(timerName: string, timerText: string, time: string,
   position: string, index: number) {
 
   const refNodeName: string = position === "right" ? "source-image-area-" + index.toString() : "canvas-area-" + index.toString();
-  const refNode = figma.currentPage.findAll(n => n.name === refNodeName);
+  const refNode = playPage.findAll(n => n.name === refNodeName);
 
   const timer = figma.createText();
   timer.name = timerName;
   timer.characters = timerText + time;
   timer.x = refNode[0] !== null ? refNode[0].x : position === "right" ? right_x : left_x;
-  timer.y = refNode[0] !== null ? refNode[0].y - 253 : timer_y(index);
+  timer.y = refNode[0] !== null ? refNode[0].y - 240 : timer_y(index);
   timer.fontSize = 100;
   timer.fills = [
     {
@@ -35,17 +38,17 @@ export function createTimer(timerName: string, timerText: string, time: string,
       visible: true,
     },
   ];
-  figma.currentPage.appendChild(timer);
+  playPage.appendChild(timer);
 
   return timer;
 }
 
 export function createSourceImage(image: Image, index: number) {
   const refNodeName: string = "source-image-area-" + index.toString()
-  const refNode = figma.currentPage.findAll(n => n.name === refNodeName);
+  const refNode = playPage.findAll(n => n.name === refNodeName);
 
   const source_img = figma.createRectangle();
-  source_img.name = "source-img";
+  source_img.name = "source-img-" + index.toString();
   source_img.x = refNode[0] !== null ? refNode[0].x : right_x;
   source_img.y = refNode[0] !== null ? refNode[0].y : src_canvas_y(index);
   source_img.resize(1000, 1000);
@@ -57,21 +60,21 @@ export function createSourceImage(image: Image, index: number) {
     },
   ];
   source_img.visible = false;
-  figma.currentPage.appendChild(source_img);
+  playPage.appendChild(source_img);
 
   return source_img;
 }
 
 export function createCanvas(index: number) {
   const refNodeName: string = "canvas-area-" + index.toString()
-  const refNode = figma.currentPage.findAll(n => n.name === refNodeName);
+  const refNode = playPage.findAll(n => n.name === refNodeName);
 
   const canvas = figma.createFrame();
-  canvas.name = "canvas";
+  canvas.name = "canvas-" + index.toString();
   canvas.x = refNode[0] !== null ? refNode[0].x : left_x;
   canvas.y = refNode[0] !== null ? refNode[0].y : src_canvas_y(index);
   canvas.resize(1000, 1000);
-  figma.currentPage.appendChild(canvas);
+  playPage.appendChild(canvas);
 
   return canvas;
 }

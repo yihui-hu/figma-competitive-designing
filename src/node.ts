cff1caf90ac6f17b8b683468141c871bcdde925f
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-05-16 17:11:49 -0400

    Added functions to create header and templates for resetting board

--- src/node.ts
+++ src/node.ts
@@ -1,6 +1,6 @@
 /**
  * This file is responsible for creating the timers, source images and
- * black canvases in Figma.
+ * blank canvases in Figma.
  * 
  * `createTimer` returns a text node, 
  * `createSourceImage` returns a rectangle node with an image fill 
@@ -44,7 +44,7 @@ export function createTimer(timerName: string, timerText: string, time: string,
 }
 
 export function createSourceImage(image: Image, index: number) {
-  const refNodeName: string = "source-image-area-" + index.toString()
+  const refNodeName: string = "source-image-area-" + index.toString();
   const refNode = playPage.findAll(n => n.name === refNodeName);
 
   const source_img = figma.createRectangle();
@@ -77,4 +77,175 @@ export function createCanvas(index: number) {
   playPage.appendChild(canvas);
 
   return canvas;
+}
+
+export async function createHeader(memotime: string, playtime: string) {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  await figma.loadFontAsync({ family: "IBM Plex Mono", style: "SemiBold" });
+
+  const header = figma.createText();
+  header.fontName = {
+    family: "IBM Plex Mono",
+    style: "SemiBold"
+  }
+  header.name = "Header";
+  header.characters = `You will have ${memotime} to memorize\nthe design and ${playtime} to replicate it.`
+  header.x = 0;
+  header.y = 50;
+  header.fontSize = 100;
+  header.fills = [
+    {
+      blendMode: "NORMAL",
+      color: { r: 1, g: 1, b: 1 },
+      opacity: 1,
+      type: "SOLID",
+      visible: true,
+    },
+  ];
+  playPage.appendChild(header);
+}
+
+export async function createTemplates() {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+  const players = await figma.clientStorage.getAsync("players") ?? 5;
+
+  // Create templates for 5 players
+  for (let i = 1; i <= 5; i++) {
+    const templateLabel = createTemplateLabel(i);
+    const templateCanvas = createTemplateCanvas(i);
+    const templateSourceImage = createTemplateSourceImage(i);
+    const groupedNodes = figma.group([templateLabel, templateCanvas, templateSourceImage], playPage);
+    groupedNodes.name = "Player " + i.toString();
+
+    // Position templates accordingly
+    if (i >= 4) {
+      groupedNodes.x = 3300;
+      groupedNodes.y = 965 + (1783 * (i - 4));
+    } else {
+      groupedNodes.x = -400;
+      groupedNodes.y = 965 + (1783 * (i - 1));
+    }
+
+    // Hide extra templates given player count
+    if (i > players + 1) {
+      groupedNodes.visible = false;
+    }
+  }
+}
+
+export function createTemplateLabel(num: number) {
+  const templateNumberBg = figma.createRectangle();
+  templateNumberBg.name = "number-bg-" + num.toString();
+  templateNumberBg.resize(200, 200);
+  templateNumberBg.fills = [
+    {
+      blendMode: "NORMAL",
+      color: { r: 1, g: 1, b: 1 },
+      opacity: 1,
+      type: "SOLID",
+      visible: true,
+    },
+  ];
+  templateNumberBg.x = -400;
+  templateNumberBg.y = 965;
+
+  const templateNumberLabel = figma.createText();
+  templateNumberLabel.fontName = {
+    family: "Inter",
+    style: "Bold"
+  }
+  templateNumberLabel.name = num.toString();
+  templateNumberLabel.characters = num.toString();
+  templateNumberLabel.x = -324;
+  templateNumberLabel.y = 1004;
+  templateNumberLabel.fontSize = 100;
+  templateNumberLabel.fills = [
+    {
+      blendMode: "NORMAL",
+      color: { r: 0, g: 0, b: 0 },
+      opacity: 1,
+      type: "SOLID",
+      visible: true,
+    },
+  ];
+
+  playPage.appendChild(templateNumberBg);
+  playPage.appendChild(templateNumberLabel);
+  const groupedNodes = figma.group([templateNumberBg, templateNumberLabel], playPage);
+  groupedNodes.name = "number-group";
+  return groupedNodes;
+}
+
+export function createTemplateCanvas(num: number) {
+  const templateCanvas = figma.createRectangle();
+  templateCanvas.name = "canvas-area-" + num.toString();
+  templateCanvas.resize(1000, 1000);
+  templateCanvas.fills = [];
+  templateCanvas.strokes = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 }, opacity: 1 }];
+  templateCanvas.dashPattern = [100, 100];
+  templateCanvas.x = 0;
+  templateCanvas.y = 965;
+  templateCanvas.strokeWeight = 10;
+  templateCanvas.strokeCap = "ROUND";
+  templateCanvas.strokeJoin = "ROUND";
+
+
+  const templateCanvasLabel = figma.createText();
+  templateCanvasLabel.name = "Canvas";
+  templateCanvasLabel.characters = "Canvas";
+  templateCanvasLabel.x = 326;
+  templateCanvasLabel.y = 1405;
+  templateCanvasLabel.fontSize = 100;
+  templateCanvasLabel.fills = [
+    {
+      blendMode: "NORMAL",
+      color: { r: 1, g: 1, b: 1 },
+      opacity: 1,
+      type: "SOLID",
+      visible: true,
+    },
+  ];
+
+  playPage.appendChild(templateCanvas);
+  playPage.appendChild(templateCanvasLabel);
+  const groupedNodes = figma.group([templateCanvas, templateCanvasLabel], playPage);
+  groupedNodes.name = "canvas-group";
+  return groupedNodes;
+}
+
+export function createTemplateSourceImage(num: number) {
+  const templateSourceImage = figma.createRectangle();
+  templateSourceImage.name = "source-image-area-" + num.toString();
+  templateSourceImage.resize(1000, 1000);
+  templateSourceImage.fills = [];
+  templateSourceImage.strokes = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 }, opacity: 0.3 }];
+  templateSourceImage.dashPattern = [100, 100];
+  templateSourceImage.x = 1300;
+  templateSourceImage.y = 965;
+  templateSourceImage.strokeWeight = 10;
+  templateSourceImage.strokeCap = "ROUND";
+  templateSourceImage.strokeJoin = "ROUND";
+
+  const templateSourceImageLabel = figma.createText();
+  templateSourceImageLabel.name = "Screenshot";
+  templateSourceImageLabel.characters = "Screenshot";
+  templateSourceImageLabel.x = 1531;
+  templateSourceImageLabel.y = 1405;
+  templateSourceImageLabel.fontSize = 100;
+  templateSourceImageLabel.fills = [
+    {
+      blendMode: "NORMAL",
+      color: { r: 1, g: 1, b: 1 },
+      opacity: 0.3,
+      type: "SOLID",
+      visible: true,
+    },
+  ];
+
+  playPage.appendChild(templateSourceImage);
+  playPage.appendChild(templateSourceImageLabel);
+  const groupedNodes = figma.group([templateSourceImage, templateSourceImageLabel], playPage);
+  groupedNodes.name = "source-image-group";
+  return groupedNodes;
 }
